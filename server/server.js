505f3e724481614f6d9c0df7ab80c768e554da1d
chinmay <chichan23603@gmail.com>
2026-02-01 15:24:58 +0530

    Fix duplicate path import crash

--- server/server.js
+++ server/server.js
@@ -1,46 +1,27 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-import path from "path";
-import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../client")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
-});
-
-// ✅ IMPORTANT: Serve client folder
-app.use(express.static(path.join(__dirname, "../client")));
-
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("drawing_step", (data) => {
-    socket.broadcast.emit("drawing_step", data);
-  });
-
-  socket.on("undo", () => {
-    io.emit("redraw_all");
-  });
+  console.log("User connected");
 
   socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    console.log("User disconnected");
   });
 });
-
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
+  console.log("Server running on", PORT);
 });
-
-
